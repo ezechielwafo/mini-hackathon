9bba0f6a843af6c0cb96a5fcf8aac6a4d4164a44
ezechielwafo <michaeliezechiel@gmail.com>
2025-05-29 14:14:18 +0200

    configuration du frontend et des creation de l'application en sorte qu'il fonctionnent dans ma machine

--- server/server_config/settings.py
+++ server/server_config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -127,7 +127,7 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Ajoutez cette configuration pour DRF (peut être ajustée plus tard)
